732d88ec301de142acb06feedfdfb84dc3dc5285
Rakshith <rakshithcodes@outlook.com>
2026-08-24 21:43:28 +0530

    python basics

---
+++ 01_Python_Basics/07_Avg&Square.py
@@ -0,0 +1,6 @@
+a = int(input("Enter a number : "))
+b = int(input("Enter another number : "))
+avg = (a + b) / 2
+square = avg ** 2
+print("Average of", a, "and", b, "is", avg) 
+print("Square of the average is", square)
