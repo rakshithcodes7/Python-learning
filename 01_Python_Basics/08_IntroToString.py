51d759a59cfb61444fb97ea0fc05b3e1e2c110e8
Rakshith <rakshithcodes@outlook.com>
2026-08-24 21:46:26 +0530

    python basics

---
+++ 01_Python_Basics/08_IntroToString.py
@@ -0,0 +1,9 @@
+name = "Rakx"
+print(name)
+print(type(name)) #This gives the type of the variable, in this case it is string
+
+length = len(name) #This gives length of the string
+print(length) 
+
+nameshort = name[0:3] #This gives the substring of the string from index 0 to 2
+print(nameshort)
